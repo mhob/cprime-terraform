540e3fc21ddb9757ae5552568105a34af09c21ef
Matt Hobson <1750074-mhobson@users.noreply.gitlab.com>
2021-09-14 11:37:42 -0700

    completed lab 3.1

--- main.tf
+++ main.tf
@@ -1,22 +1,65 @@
 terraform {
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 2.3.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.40, < 3.0"
     }
   }
-  required_version = "~> 1.0.0"
   backend "azurerm" {
     resource_group_name = "terraform-course-backend"
     container_name      = "tfstate"
     key                 = "cprime.terraform.labs.tfstate"
   }
+  required_version = "~> 1.0.0"
 }
 
-provider "random" {
+provider "azurerm" {
+  features {}
+  # Below may be needed for lab environment
+  skip_provider_registration = true
 }
 
-resource "random_integer" "number" {
-  min = 1
-  max = 100
+resource "azurerm_resource_group" "lab" {
+  name     = "aztf-labs-rg"
+  location = "westus2"
+  tags = {
+    Environment = "Lab"
+    Project     = "AZTF Training"
+  }
+}
+
+resource "azurerm_virtual_network" "lab" {
+  name                = "aztf-labs-vnet"
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.lab.name
+  address_space       = ["10.0.0.0/16"]
+  tags = {
+    Environment = "Lab"
+    Project     = "AZTF Training"
+  }
+}
+
+resource "azurerm_subnet" "lab-public" {
+  name                 = "aztf-labs-subnet-public"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet" "lab-private" {
+  name                 = "aztf-labs-subnet-private"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "lab-public" {
+  name                = "aztf-labs-public-sg"
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.lab.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "lab-public" {
+  subnet_id                 = azurerm_subnet.lab-public.id
+  network_security_group_id = azurerm_network_security_group.lab-public.id
 }
